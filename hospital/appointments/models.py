97c6ba58973e052f1e1f73102e6e9f5d2d432068
shahryaralam688 <shahryaralamft@gmail.com>
2024-07-10 12:28:30 +0500

    modify appointments app

--- hospital/appointments/models.py
+++ hospital/appointments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from patients.models import Patients
+from doctors.models import Doctors
 
 class Appointment(models.Model):
     
 	appointment_id =models.CharField(max_length=20)
-    # patient_id = 
-    # token_number =
-    # doctor_name
-    # problem
+    patient_id = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    token_number =models.CharField(max_length=10)
+    doctor_name = models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    problem = models.CharField(max_length=20)
 # Create your models here.
